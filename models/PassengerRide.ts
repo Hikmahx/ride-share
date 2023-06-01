8da97d5e59b3b597a10e3cd3040d91f6976459e6
Hikmahx <hikmayousuph@gmail.com>
2023-06-01 18:09:35 +0100

    driver ride and passenger ride models

---
+++ models/PassengerRide.ts
@@ -0,0 +1,25 @@
+import { Document, Schema, model } from "mongoose";
+
+const passengerRideSchema = new Schema(
+  {
+    passenger: { type: Schema.Types.ObjectId, ref: "User", required: true }, 
+    driver: { type: Schema.Types.ObjectId, ref: "DriverRide" },
+    pickupLocation: { type: String, required: true },
+    dropoffLocation: { type: String, required: true },
+    numberOfPassengers: {
+      type: Number,
+      required: true,
+    },
+    price: { type: Number, required: true },
+    status: {
+      type: String,
+      enum: ["requested", "accepted", "completed", "cancelled"],
+      default: "requested",
+    },
+  },
+  { timestamps: true }
+);
+
+const PassengerRide = model("PassengerRide", passengerRideSchema);
+
+export default PassengerRide;
