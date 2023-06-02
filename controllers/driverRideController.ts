4f16243e60bb3466970fddee08e1b7df05251755
Hikmahx <hikmayousuph@gmail.com>
2023-06-02 17:44:06 +0100

    create vehicle controller

--- controllers/driverRideController.ts
+++ controllers/driverRideController.ts
@@ -18,6 +18,64 @@ interface AuthRequest extends Request {
 // POST REQUESTS
 // ---
 
+// @route    POST api/vehicles
+// @desc     Create a new vehicle
+// @access   Private (Driver)
+export const createVehicle = async (req: AuthRequest, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const {
+      make,
+      model,
+      year,
+      color,
+      plateNumber,
+      description,
+      seats,
+      avatar,
+      available,
+      location,
+    } = req.body;
+
+    // Check if the driver already has a vehicle
+    const existingVehicle = await Vehicle.findOne({ driver: req.user?.id });
+
+    if (existingVehicle) {
+      return res.status(400).json({
+        error:
+          "Driver already has a vehicle. Update your vehicle if you need to",
+      });
+    }
+
+    // Create a new vehicle
+    const vehicle = new Vehicle({
+      driver: req.user?.id,
+      make,
+      model,
+      year,
+      color,
+      plateNumber,
+      description,
+      seats,
+      avatar,
+      available,
+      location,
+    });
+
+    // Save the vehicle
+    await vehicle.save();
+
+    res.status(200).json(vehicle);
+  } catch (err: any) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+};
+
 // @route    POST api/rides
 // @desc     Create a new ride
 // @access   Private (Driver)
