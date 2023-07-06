a8e987e3d5db90042e79004a691c2d84be7e4a0f
Hikmahx <hikmayousuph@gmail.com>
2023-07-06 20:37:07 +0100

    base routes

--- ride_share/urls.py
+++ ride_share/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('auth.urls')),
+    path('api/rides/', include('driver.urls')),
+    path('api/passengers/', include('passenger.urls')),
+    path('api/vehicles/', include('vehicle.urls'))
 ]
