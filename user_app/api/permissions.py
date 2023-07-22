31eeb1d7e2ac32b819ed5f466f4a19e05d100873
Hikmahx <hikmayousuph@gmail.com>
2023-07-22 19:39:02 +0100

    permissions updated

--- user_app/api/permissions.py
+++ user_app/api/permissions.py
@@ -1,31 +1,31 @@
 from rest_framework import permissions
 
+
 class IsDriver(permissions.BasePermission):
-  """
-  Check if the user is a driver
-  """ 
-  
-  def has_permission(self, request, view):
-    return request.user.role == "driver"
-  
+    """
+    Check if the user is a driver
+    """
+
+    def has_permission(self, request, view):
+          if request.user.role == "driver":
+              return True
+          else:
+              self.message = "User must be a driver to have a vehicle."
+              return False
+
 # class IsDriverOrReadOnly(permissions.BasePermission):
-  
+
 #     def has_permission(self, request, view):
 #       if request.method in permissions.SAFE_METHODS:
 #         return True
 #       else:
 #         return request.user.role == "driver"
-      
-      
-class IsVerified(permissions.BasePermission):
-  """
-  Check user's verified status
-  """
-  
-  def has_permission(self, request, view):
-    return request.user.verified
-  
 
-  
-  
-      
+
+class IsVerified(permissions.BasePermission):
+    """
+    Check user's verified status
+    """
+
+    def has_permission(self, request, view):
+        return request.user.verified
