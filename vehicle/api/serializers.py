f832433691a8c4d1390f9cbcea1a7bd62bcb6dd5
Hikmahx <hikmayousuph@gmail.com>
2023-07-24 20:20:21 +0100

    vehicle app updated

--- vehicle/api/serializers.py
+++ vehicle/api/serializers.py
@@ -1,33 +1,31 @@
 from rest_framework import serializers
 from ..models import Vehicle
 
+
 class NewVehicleSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Vehicle
-    fields = '__all__'
-    
-    def validate(self, value):
-      # Get the user from the request
-      user = self.context['request'].user
-      
-      # Check if the user already has a vehicle
-      existing_vehicle = Vehicle.objects.filter(driver=user).exists()
-      if existing_vehicle:
-            raise serializers.ValidationError("User already has a vehicle.")
-      return value
-    
-      def create(self, validated_data):
-        # Get the user from the request context
-        user = self.context['request'].user
-        
+    class Meta:
+        model = Vehicle
+        fields = '__all__'
+        read_only_fields = ('driver',)  # Set 'driver' field as read-only
+
+    def create(self, validated_data):
+        # Get the user from the request
+        driver = self.context['request'].user
+
+        # Check if the user already has a vehicle
+        existing_vehicle = Vehicle.objects.filter(driver=driver).exists()
+        if existing_vehicle:
+            raise serializers.ValidationError(
+                {'error': "User already has a vehicle."})
+
         # Set the authenticated user as the driver of the new vehicle
-        validated_data['driver'] = user
-        
+        validated_data['driver'] = driver
+
         # Create and return the new vehicle instance
         return Vehicle.objects.create(**validated_data)
-    
+
 
 class VehicleSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Vehicle
-    fields = '__all__'
+    class Meta:
+        model = Vehicle
+        fields = '__all__'
--- vehicle/api/views.py
+++ vehicle/api/views.py
@@ -5,10 +5,16 @@ from vehicle.models import Vehicle
 from .serializers import VehicleSerializer, NewVehicleSerializer
 from user_app.api.permissions import IsDriver
 
+
 class CreateVehicleAPI(generics.CreateAPIView):
     queryset = Vehicle.objects.all()
     serializer_class = NewVehicleSerializer
     permission_classes = [IsAuthenticated, IsDriver]
+
+    def perform_create(self, serializer):
+        # Set the authenticated user as the driver (from the model) of the new vehicle
+        serializer.save(driver=self.request.user)
+
 
 class GetDriverVehicleAPI(generics.RetrieveAPIView):
     queryset = Vehicle.objects.all()
@@ -17,6 +23,7 @@ class GetDriverVehicleAPI(generics.RetrieveAPIView):
 
     def get_object(self):
         return Vehicle.objects.filter(driver=self.request.user.id).first()
+
 
 class UpdateVehicleAPI(generics.UpdateAPIView):
     queryset = Vehicle.objects.all()
