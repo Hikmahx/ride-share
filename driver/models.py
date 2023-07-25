050212f2e63d3581f3f16d0771a674b5550b04ee
Hikmahx <hikmayousuph@gmail.com>
2023-07-25 23:24:41 +0100

    passenger ride and driver ride model

--- driver/models.py
+++ driver/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+from user_app.models import Profile
+
+class DriverRide(models.Model):
+    passengers = models.ForeignKey('passenger.PassengerRide', on_delete=models.CASCADE, related_name='driver_rides')
+    driver = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    vehicle = models.ForeignKey('vehicle.Vehicle', on_delete=models.CASCADE)
+    pickupLocation = models.CharField(max_length=150)
+    dropoffLocation = models.CharField(max_length=150)
+    seatsAvailable = models.IntegerField(validators=[MinValueValidator(1)])
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"Driver Ride - Driver: {self.driver}, Vehicle: {self.vehicle}, Seats Available: {self.seatsAvailable}"
--- passenger/models.py
+++ passenger/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from driver.models import DriverRide
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+from user_app.models import Profile
+
+class PassengerRide(models.Model):
+    STATUS_CHOICES = [
+        ('requested', 'Requested'),
+        ('accepted', 'Accepted'),
+        ('completed', 'Completed'),
+        ('cancelled', 'Cancelled'),
+    ]
+
+    passenger = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='passenger_rides')
+    driver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='driver_rides')
+    ride = models.ForeignKey(DriverRide, on_delete=models.CASCADE)
+    pickupLocation = models.CharField(max_length=150)
+    dropoffLocation = models.CharField(max_length=150)
+    numberOfPassengers = models.IntegerField(validators=[MinValueValidator(1)])
+    price = models.IntegerField()
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='requested')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Passenger Ride - Passenger: {self.passenger}, Driver: {self.driver}, Status: {self.status}"
